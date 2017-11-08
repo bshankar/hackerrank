5d7b9b0b2024334c73819304962a8977dfb8eb28
Bhavani Shankar <ebhavanishankar@gmail.com>
2017-11-08 14:41:54 +0530

    add solution to sum_vs_xor

---
+++ src/bit_manipulation/sum_vs_xor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+typedef unsigned long ul;
+using namespace std;
+
+int main() {
+  ul n;
+  cin >> n;
+
+  if (!n) cout << 1 << "\n";
+  else {
+    ul one = 1;
+    ul allFilled = (one << (64 - __builtin_clzl(n))) - 1;
+    cout <<  (one << __builtin_popcountl(allFilled ^ n)) << "\n";
+  }
+}
